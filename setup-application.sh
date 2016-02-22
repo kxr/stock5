d6fdaf5ea5f98ac302adbdb44f3be50b84cdff13
Khizer Naeem <khizernaeem@gmail.com>
2016-02-22 21:18:56 +0400

    init

--- setup-application.sh
+++ setup-application.sh
@@ -5,7 +5,7 @@ export COMPOSER_HOME="/root"
 cd /var/www/stock5
 
 #scl enable php55 '/bin/composer install --no-dev --optimize-autoloader --no-interaction' &>> /tmp/cd_debug
-git pull
+#git pull
 scl enable php55 'php artisan cache:clear' &>> /tmp/cd_debug
 
 chown -R nginx:nginx /var/www/stock5/storage /var/www/stock5/bootstrap/cache;
