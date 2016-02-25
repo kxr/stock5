90ffcb0ca7addda928a935fc9ffeff179cd607ed
Khizer Naeem <khizernaeem@gmail.com>
2016-02-25 10:51:24 +0400

    init

---
+++ database/migrations/2016_02_25_061133_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('group_id')->unsigned();
+            $table->string('name');
+            $table->string('type');
+            $table->string('color');
+            $table->string('make');
+            $table->string('size');
+            $table->string('note');
+
+            $table->foreign('group_id')->references('id')->on('items');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('items');
+    }
+}
