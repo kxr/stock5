ed86a359dac15039741b3629bf5c8747c7b154f3
Khizer Naeem <khizernaeem@gmail.com>
2016-02-27 18:13:34 +0400

    init

--- database/migrations/2016_02_22_163509_create_stock5_table.php
+++ database/migrations/2016_02_22_163509_create_stock5_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVendorsTable extends Migration
+class CreateStock5Table extends Migration
 {
     /**
      * Run the migrations.
